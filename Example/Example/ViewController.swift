b622ada65dd44eaaf4d5a0e4c5507e5da25b9c89
Matthias Schlemm <matthias@sixpolys.com>
2016-09-24 12:33:25 +0200

    swift 3.0 sytanx

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         // The ColorPickerController handels lets you connect a colorWell, a colorPicker and a huepicker. The ColorPickerController takes care of propagating changes between the individual components.
         pickerController = ColorPickerController(svPickerView: colorPicker!, huePickerView: huePicker!, colorWell: colorWell!)
         //Instead of setting an initial color on all 3 ColorPicker components, the ColorPickerController lets you set a color which will be propagated to the individual components.
-        pickerController?.color = UIColor.redColor()
+        pickerController?.color = UIColor.red
         
         /* you shoudln't interact directly with the individual components unless you want to do customization of the colorPicker itself. You can provide a closure to the pickerController, which is going to be invoked when the user is changing a color. Notice that you will receive intermediate color changes. You can use these by coloring the object the User is actually trying to color, so she/he gets a direct visual feedback on how a color changes the appearance of an object of interet. The ColorWell aids in this process by showing old and new color side-by-side.
         */
